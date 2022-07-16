89aeb86c6f8966158362e1f09a9e4f10ad5d2a53
KubraAkkaya <akkaya32kubra@gmail.com>
2022-07-16 21:46:54 +0300

    06.07.2022

---
+++ BusinessLayer/Concrete/FaturaManager.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class FaturaManager : IFaturaService
+    {
+        IFaturaDal _faturaDal;
+        public FaturaManager(IFaturaDal faturaDal)
+        {
+            _faturaDal = faturaDal;
+        }
+        public List<Fatura> GetList()
+        {
+            return _faturaDal.List();
+        }
+    }
+}
---
+++ FirinTakip/Controllers/FaturaController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirinTakip.Controllers
+{
+    public class FaturaController : Controller
+    {
+        FaturaManager fm = new FaturaManager(new EfFaturaDal());
+        public ActionResult Index()
+        {
+            var faturaValues = fm.GetList();
+            return View(faturaValues);
+        }
+    }
+}
